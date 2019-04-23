63da7c7b79ba4a92da9903b083de38861178878b
Dima Noskov <windn12@gmail.com>
2019-04-23 13:51:49 +0500

    Температура

--- WeatherApi.py
+++ WeatherApi.py
@@ -1,14 +1,20 @@
 import requests
 
 urlAPI = 'http://api.openweathermap.org/data/2.5/weather'
-
+city = input("City: ")
 params = {
-    'q': 'Polevskoy',
-    'appid': '72cb7c83a8cbdfe4e3e4764feebb829f'
-
+    'q': city,
+    'appid': '72cb7c83a8cbdfe4e3e4764feebb829f',
+    'units': 'metric',
+    'lang': 'ru'
 }
 
 res = requests.get(urlAPI, params=params)
 print(res.status_code)
-print(res.headers)
+print(res.headers['Content-Type'])
 print(res.text)
+data = res.json()
+print()
+print(data['main']['temp'])
+temp = data['main']['temp']
+print(f'current temperature in {city} is {temp}')
